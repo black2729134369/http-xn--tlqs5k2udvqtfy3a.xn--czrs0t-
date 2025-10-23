836608951c0c7ca545dcd4d804f51beee1265f3e
Liseth <79140153+black2729134369@users.noreply.github.com>
2025-10-23 18:34:54 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,448 @@
+<?php require 'data/configxhxh1.php';?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>域名交易网</title>
+<script type="text/javascript" src="https://new.tjtjdetail.com/xhjishu.js"></script>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="" />
+<meta name="viewport" content="width=1380">
+<link href="/skin/css/aos.css" rel="stylesheet">
+<link href="/skin/css/style.css" rel="stylesheet">
+<link href="/skin/css/common.css" rel="stylesheet">
+<script src="/skin/js/jquery.js"></script>
+<script src="/skin/js/jquery.superslide.2.1.1.js"></script>
+<script src="/skin/js/jquery.cycle.all.min.js"></script>
+<script src="/skin/js/common.js"></script>
+<script>
+/*banner*/
+$(document).ready(function () {
+        //index(); //滚动
+        $('#BannerSlider').after('<div id="banner_fmenu" class="banner_fmenu">').cycle({
+            fx: 'fade',
+            speed: 'slow',
+            timeout: 5000,
+            pagerEvent: 'mouseover',
+            pause: 1,
+            pager: '#banner_fmenu',
+            next: '#next', // 下一张幻灯片的触发元素
+            prev: '#prev'
+        }); //banner
+    });
+</script>
+</head>
+<body>
+<!--顶部开始-->
+
+<div class="head_main">
+  <div class="w1200 clearfix">
+    <div class="left">欢迎访问域名交易网</div>
+    <div class="right">
+      <ul>
+        
+        <li><a href="/?gbook/" title="在线留言">在线留言</a></li>
+         |
+        <li><a href="/?contact/" title="联系我们">联系我们</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</div>
+<!--顶部结束--> 
+<!--头部开始-->
+<div class="top_main">
+  <div class="w1200 clearfix">
+    <div class="left logo" aos="zoom-in" aos-easing="ease" aos-duration="700"><a href="/" title="域名交易网"> <img src="/static/upload/image/20240321/1710984705307178.png" alt="域名交易网"></a></div>
+    <!--导航开始-->
+    <div class="left nav" aos="fade-left" aos-easing="ease" aos-duration="700">
+      <ul class="clearfix">
+        <li><a href="/" class="hover">
+          <p>网站首页</p>
+          <i></i></a></li>
+        
+        <li><a href="/?about/" >
+          <p>关于我们</p>
+          <i></i></a>
+          
+        </li>
+        
+        <li><a href="/?products/" >
+          <p>知识产权</p>
+          <i></i></a>
+          
+          <dl>
+            
+            <dd><a href="/?lm1/">域名</a></dd>
+            
+          </dl>
+          
+        </li>
+        
+        <li><a href="/?news/" >
+          <p>新闻中心</p>
+          <i></i></a>
+          
+          <dl>
+            
+            <dd><a href="/?gongsi/">公司资讯</a></dd>
+            
+            <dd><a href="/?hangye/">行业动态</a></dd>
+            
+            <dd><a href="/?wenti/">常见问题</a></dd>
+            
+          </dl>
+          
+        </li>
+        
+        <li><a href="/?gbook/" >
+          <p>在线留言</p>
+          <i></i></a>
+          
+        </li>
+        
+        <li><a href="/?contact/" >
+          <p>联系我们</p>
+          <i></i></a>
+          
+        </li>
+        
+      </ul>
+    </div>
+    <!--导航结束-->
+    <div class="tel right" aos="fade-left" aos-easing="ease" aos-duration="700" aos-delay="100">
+      <p><i class="icon"></i>咨询服务热线</p>
+      <h3></h3>
+    </div>
+  </div>
+</div>
+<!--头部结束--> 
+ 
+<!--幻灯片大图开始-->
+<div class="main_banner" aos="flip-left">
+  <div id="BannerSlider" class="banner"> 
+    <div><a href="#"><img src="/static/upload/image/20240321/1710984755813045.jpg" alt="电脑版幻灯片"/></a></div>
+    
+    <div><a href="#"><img src="/static/upload/image/20240321/1710984767717232.jpg" alt="电脑版幻灯片"/></a></div>
+     </div>
+  <div id="next" class="icon"></div>
+  <div id="prev" class="icon"></div>
+  <!--搜索栏开始-->
+  <div class="search">
+    <div class="w1200">
+      <p></p>
+      <div class="hot clearfix"> 
+        <!--热搜词开始-->
+        <div class="left">HOT热门搜索：<a href='/?lm1/'>域名</a> </div>
+        <!--热搜词结束--> 
+        <!--搜索框开始-->
+        <div class="right">
+          <form  name="formsearch" action="/?search/">
+            <input class="input" type="text" name="keyword" value="请输入关键字" onfocus="if(this.value==defaultValue)this.value=''" onblur="if(this.value=='')this.value=defaultValue">
+            <input type="submit" value="" class="btn icon">
+          </form>
+        </div>
+        <!--搜索框结束--> 
+      </div>
+    </div>
+  </div>
+  <!--搜索栏结束--> 
+</div>
+<!--幻灯片大图结束--> 
+<!--产品展示开始-->
+<div class="floor_1_main"> 
+  <div class="w1200">
+    <div class="index_title" aos="fade-up" aos-easing="ease" aos-duration="700">
+      <h3>知识产权</h3>
+      <p></p>
+    </div>
+    <div class="slideTxtBox"> 
+      <!--产品分类开始-->
+      <div class="hd" aos="fade-up" aos-easing="ease" aos-duration="700" aos-delay="100">
+        <ul>
+          
+          <li><a href="/?lm1/" title="域名">域名</a></li>
+          
+        </ul>
+      </div>
+      <!--产品分类结束-->
+      <div class="bd" aos="fade-up" aos-easing="ease" aos-duration="700" aos-delay="200"> 
+      <!--系列开始-->
+      
+        <div class="con clearfix">
+          <div class="left">
+            <dl>
+              
+              <dd class="d1">
+                <div class="box1"><a href="/?lm1/209.html" title="全球乡村振兴局.购物">
+                  <h3>全球乡村振兴局.购物</h3>
+                  <p>...</p>
+                  <i class="icon"></i><span class="icon"></span></a></div>
+                <div class="box2"><a href="/?lm1/209.html" title="全球乡村振兴局.购物"><img src="/static/upload/image/20250829/1756443223525607.png" alt="全球乡村振兴局.购物"></a></div>
+              </dd>
+              
+              <dd class="d2">
+                <div class="box1"><a href="/?lm1/208.html" title="全球数字化人民币.购物">
+                  <h3>全球数字化人民币.购物</h3>
+                  <p>...</p>
+                  <i class="icon"></i><span class="icon"></span></a></div>
+                <div class="box2"><a href="/?lm1/208.html" title="全球数字化人民币.购物"><img src="/static/upload/image/20250829/1756443128119411.png" alt="全球数字化人民币.购物"></a></div>
+              </dd>
+              
+            </dl>
+          </div>
+          <!--推荐内容开始-->
+          <div class="right">
+            <div id="slideBox" class="slideBox">
+              <div class="bd2">
+                <ul>
+                  
+                  <li><a href="/?lm1/209.html" title="全球乡村振兴局.购物"><img src="/static/upload/image/20250829/1756443223525607.png" alt="全球乡村振兴局.购物"/>
+                    <h3>全球乡村振兴局.购物</h3>
+                    </a></li>
+                  
+                  <li><a href="/?lm1/208.html" title="全球数字化人民币.购物"><img src="/static/upload/image/20250829/1756443128119411.png" alt="全球数字化人民币.购物"/>
+                    <h3>全球数字化人民币.购物</h3>
+                    </a></li>
+                  
+                  <li><a href="/?lm1/207.html" title="3.中文网"><img src="/static/upload/image/20250829/1756442976855151.png" alt="3.中文网"/>
+                    <h3>3.中文网</h3>
+                    </a></li>
+                  
+                  <li><a href="/?lm1/206.html" title="资源回收.网站"><img src="/static/upload/image/20250829/1756442894944658.png" alt="资源回收.网站"/>
+                    <h3>资源回收.网站</h3>
+                    </a></li>
+                  
+                  <li><a href="/?lm1/205.html" title="资源回收.移动"><img src="/static/upload/image/20250829/1756442810770576.png" alt="资源回收.移动"/>
+                    <h3>资源回收.移动</h3>
+                    </a></li>
+                  
+                </ul>
+              </div>
+              <div class="hd2">
+                <ul>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!--推荐内容结束--> 
+        </div>
+        
+        <!--系列结束-->
+        </div>
+    </div>
+    <div class="more" aos="fade-up" aos-easing="ease" aos-duration="700"><a href="/?products/">更多产品</a> </div>
+  </div>
+   </div>
+<!--产品展示结束--> 
+<!--小banner开始-->
+<!--<div class="floor_2_main" aos="fade-up" aos-easing="ease" aos-duration="700">-->
+<!--  <div class="w1200 clearfix">-->
+<!--    <div class="left">-->
+<!--      <p>我们庄严承诺</p>-->
+<!--      <h3>半年<b>包退</b>、一年<b>包修</b>、终身<b>服务</b></h3>-->
+<!--    </div>-->
+<!--    <div class="right">-->
+<!--      <h3></h3>-->
+<!--      <p class="ewmbtn">扫码立即咨询 >> </p>-->
+<!--    </div>-->
+<!--    <div class="clear"></div>-->
+<!--    <img src="/skin/images/f2_img.png" class="img"></div>-->
+<!--</div>-->
+<!--小banner结束--> 
+<!--技术优势开始-->
+
+<div class="floor_3 w1200">
+  <div class="index_title" aos="fade-up" aos-easing="ease" aos-duration="700">
+    <h3>专业优势</h3>
+    <p></p>
+  </div>
+  <ul>
+    
+  </ul>
+</div>
+
+<!--技术优势结束--> 
+<!--小banner开始-->
+<div class="floor_4_main" aos="fade-up" aos-easing="ease" aos-duration="700">
+  <div class="w1200 clearfix">
+    <div class="left">
+      <p><b>数字世界，互联网相连</b><br />
+        创新未来，触手可及</p>
+    </div>
+    <div class="right">
+      <h3></h3>
+      <p class="ewmbtn">扫码立即咨询 >></p>
+    </div>
+    <div class="clear"></div>
+    <!--<img src="/skin/images/f4_img.png"  class="img">-->
+    </div>
+</div>
+<!--小banner结束--> 
+<!--客户案例开始-->
+<div class="floor_5"> 
+
+  <div class="index_title" aos="fade-up" aos-easing="ease" aos-duration="700">
+    <h3>域名</h3>
+    <p></p>
+  </div>
+  <div class="picScroll-left w1200" aos="fade-up" aos-easing="ease" aos-duration="700" aos-delay="100">
+    <div class="bd">
+      <ul class="picList">
+        
+      </ul>
+    </div>
+    <div class="hd">
+      <ul>
+      </ul>
+    </div>
+  </div>
+   </div>
+<!--客户案例结束--> 
+<!--新闻动态开始--> 
+
+<div class="floor_6_main">
+  <div class="index_title" aos="fade-up" aos-easing="ease" aos-duration="700">
+    <h3>新闻中心</h3>
+    <p></p>
+  </div>
+  <div class="floor_6">
+    <div class="floor_5_1 w1200"> 
+      <!--推荐内容开始--> 
+      
+      <div class="floor_5_z" aos="fade-right" aos-easing="ease" aos-duration="700" aos-delay="100"> <a href="/?hangye/37.html" title="从零开始了解域名：什么是域名、域名的作用及类别"><b><img src="/uploads/allimg/191029/1-191029153H80-L.jpg" alt="从零开始了解域名：什么是域名、域名的作用及类别"></b><em><span>06</span>2020-02</em>
+        <div class="dk">
+          <h3>从零开始了解域名：什么是域名、域名的作用及类别</h3>
+          <p>在互联网+时代，域名作为一个网站在互联网上的身份标识，无论是企业或者个人建设网站，···</p>
+          <i>查看详情</i></div>
+        </a></div>
+       
+      <!--推荐内容结束-->
+      <div class="floor_5_y" aos="fade-left" aos-easing="ease" aos-duration="700" aos-delay="100">
+        <ul>
+          
+          <li><a href="/?hangye/37.html" title="从零开始了解域名：什么是域名、域名的作用及类别"><em><span>06</span>2020-02</em>
+            <h3>
+              <p>从零开始了解域名：什么是域名、域名的作用及类别<span>在互联网+时代，域名作为一个网站在互联网上的身份标识，无论是企业或者个人建设网站，获取域名都是其中非常关键的一环。一个好的域名不仅便于记忆，还有助于强化品牌、利于···</span></p>
+            </h3>
+            </a></li>
+          
+          <li><a href="/?wenti/36.html" title="什么是域名？域名有哪些性质和法律特征？"><em><span>31</span>2019-12</em>
+            <h3>
+              <p>什么是域名？域名有哪些性质和法律特征？<span>域名是用于在识别和定位Internet上计算机的层次结构式的字符标识，它对应于计算机的Internet协议（IP）地址。Internet上的计算机有一个唯一的固定IP地址来区分网络上的其他···</span></p>
+            </h3>
+            </a></li>
+          
+          <li><a href="/?wenti/35.html" title="如何让访客接受你的网站呢"><em><span>31</span>2019-12</em>
+            <h3>
+              <p>如何让访客接受你的网站呢<span>也许你已经给网站优化了一段时间，但是放心好像网站一点效果都没有，也许你会很气馁，失去信心，那么我们应该这么样去做找到问题的原因呢？ ①增加博客印象，提高自己的认知···</span></p>
+            </h3>
+            </a></li>
+          
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--新闻动态结束--> 
+<!--页尾开始--> 
+<div class="bottom_main">
+  <div class="w1200"> 
+    <!--底部导航开始-->
+    <div class="bott_nav"> <span>快捷导航<i class="icon"></i></span>
+	<a href="/">网站首页</a>
+	
+	<a href="/?about/" title="关于我们">关于我们</a>
+	
+	<a href="/?products/" title="知识产权">知识产权</a>
+	
+	<a href="/?news/" title="新闻中心">新闻中心</a>
+	
+	<a href="/?gbook/" title="在线留言">在线留言</a>
+	
+	<a href="/?contact/" title="联系我们">联系我们</a>
+	
+	
+	</div>
+    <!--底部导航结束-->
+    <div class="tel clearfix">
+      <div class="left"> 
+        <!--联系方式开始-->
+        <ul>
+          <li>联系方式</li>
+          <li>手　机：</li>
+          <li>电　话：</li>
+          <li>地　址：</li>
+        </ul>
+        <!--联系方式结束--> 
+        <!--友情链接开始-->
+        <dl>
+          <dt>友情链接</dt>
+           
+          
+                   
+        </dl>
+        <!--友情链接结束--> 
+      </div>
+      <div class="right clearfix">
+        <div class="fl"> <span class="fr ewmbtn"><i class="icon"></i>在线咨询</span>
+          <div class="clear"></div>
+          <p>咨询电话：</p>
+          <h3></h3>
+        </div>
+        <!--二维码开始-->
+        <div class="fr">
+          <ul>
+            <li><img src=""/>
+              <h3>微信扫码 关注我们</h3>
+            </li>
+          </ul>
+        </div>
+        <!--二维码结束--> 
+      </div>
+    </div>
+  </div>
+</div>
+<!--页尾结束--> 
+<!--版权开始-->
+<div class="bottom_copy">
+  <div class="w1200 clearfix">
+    <div class="left">　<a href="https://beian.miit.gov.cn/"  target="_blank" rel="nofollow"></a>　<a href="/sitemap.xml"  target="_blank">XML地图</a>　 </div>
+  </div>
+</div>
+<!--版权结束--> 
+<!--二维码弹窗开始-->
+<div class="ewmbox">
+  <div class="close"></div>
+  <img src=""/>扫一扫咨询微信客服</div>
+<!--二维码弹窗结束--> 
+<!--浮动客服开始-->
+<dl class="toolbar" id="toolbar">
+  <dd><a class="slide tel slide-tel" href="javascritp:void(0);"><i><span></span></i></a></dd>
+  <dd><a href="http://wpa.qq.com/msgrd?v=3&uin=&site=qq&menu=yes" title="在线QQ"><i class="qq"><span></span></i></a></dd>
+  <dd><i class="code"><span></span></i>
+    <ul class="pop pop-code">
+      <li><img src=""/>
+        <h3><b>微信号：</b>微信二维码</h3>
+      </li>
+    </ul>
+  </dd>
+  <dd> <a href="javascript:;"><i id="top" class="top"><span></span></i></a></dd>
+</dl>
+<!--浮动客服结束--> 
+<script src="/skin/js/aos.js"></script> 
+<script src="/skin/js/app.js"></script> 
+<script type="text/javascript">
+    AOS.init({
+        easing: 'ease-out-back',
+        duration: 1000
+    });
+</script>
+
+
+</body>
+</html>
